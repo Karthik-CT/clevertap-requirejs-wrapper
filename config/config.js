fd8ac846c6aafcf295b1129337009aa5ef5f5e9b
Karthik Iyer <100284000+Karthik-CT@users.noreply.github.com>
2024-01-17 15:31:15 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,6 +2,10 @@ requirejs.config({
   baseUrl: "scripts",
   paths: {
     jquery: ["https://code.jquery.com/jquery-3.6.3.min"],
-    clevertap: ["https://d2r1yp2w7bby2u.cloudfront.net/js/clevertap.min"],
+    clevertap: [
+      ("https:" == document.location.protocol
+        ? "https://d2r1yp2w7bby2u.cloudfront.net"
+        : "http://static.clevertap.com") + "/js/clevertap.min",
+    ],
   },
 });
